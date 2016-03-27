561d83b2c8bf9ce27d604ed627ea97a3f023e1db
Kirson <adam.lubanskii@gmail.com>
2016-03-27 21:47:59 +0200

    JS reset value button

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -1,5 +1,11 @@
 function getResult() {
   alert("elResult");
 }
-var elResult = document.getElementById('abc');
+var elResult = document.getElementById('result');
 elResult.onclick = getResult;
+
+function resetInput() {
+  document.getElementById("input").value = "0";;
+}
+var input = document.getElementById('cleanInput');
+input.onclick = resetInput;
