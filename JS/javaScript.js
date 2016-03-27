1a560ccc1b2cbd6ad5cac6c3c809859b3d4d683f
Kirson <adam.lubanskii@gmail.com>
2016-03-27 18:04:41 +0200

    Working JS code

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -1,6 +1,5 @@
-
-function myFunction() {
-    document.getElementById("demo").style.color = "red";
-    var x=document.getElementById("demo");
- alert(x.innerHTML);
+function getResult() {
+  alert("elResult");
 }
+var elResult = document.getElementById('abc');
+elResult.onclick = getResult;
