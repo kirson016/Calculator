67e8799df5cf1d66b817d262163f0c42891e5d49
Kirson <adam.lubanskii@gmail.com>
2016-03-27 13:08:53 +0200

    Created JS file and connected html with js

---
+++ JS/javaScript.js
@@ -0,0 +1,12 @@
+keys(){
+  innerHTML.alert("hello world");
+}
+key(){
+  innerHTML.alert("hello world");
+}
+function toCelsius() {
+    alert("AAAAAAAAAAAAA!");
+}
+document.getElementById("demo").innerHTML = toCelsius;
+ document.getElementById("demo") = alert("aaaaaaaaaaaaa");
+  alert("aaaaaaaaaaaaa") = document.getElementById("demo");
