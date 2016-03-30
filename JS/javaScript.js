424b3cade29d262255537c808dbe612a6eafa731
Kirson <adam.lubanskii@gmail.com>
2016-03-30 12:45:06 +0200

    Press enter to get result function

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -4,15 +4,22 @@ var calculator = {
   clearButoon: document.getElementById('clearButton'),
   elInput: document.getElementById('resultWindow'),
 
-  getResult: function(elInput) {
+  getResult: function() {
     calculator.elInput.value = eval(Calc.Input.value);
-
   },
 
   resetInput: function() {
     calculator.elInput.value = " ";
   },
+
+  enterFunction: function(event) {
+      if (event.keyCode == 13 || event.keyCode== 61) {
+        let result = eval(Calc.Input.value);
+      calculator.elInput.value = result;
+      }
+  },
 };
 
 calculator.elResult.onclick = calculator.getResult;
 calculator.clearButoon.onclick = calculator.resetInput;
+calculator.elInput.onkeydown = calculator.enterFunction ;
